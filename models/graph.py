66d103ebeb31c0c067f68c5b8c7b6a3891a68281
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-10-03 19:20:24 -0500

    initial state graph file

---
+++ models/graph.py
@@ -0,0 +1,59 @@
+import math
+from utils import Utils
+
+utils  = Utils()
+
+class Graph:
+    """Graph class"""   
+    def __init__(self, name, version):
+        self.name = name
+        self.version = version
+        self.verteces = []
+
+
+class Vertex:
+    """Vertex class"""
+    max_edge_length_km = 100
+    
+    def __init__(self, value, latitude, length):
+        self.value = value 
+        self.latitude = latitude
+        self.length = length 
+        self.neighbours = [] 
+        self.weights = []
+
+
+    def search_neighbours(self, graph):
+        """ Search all self vertex neighbours  and determine  
+            weight between self vertex and rest of vertices. 
+            Weight is equal to  distance between two users. 
+            If  distance between two user is less or equal
+            to max edge length in kilometres, edge does not exist in other 
+            words, conexion between two user not exist."""
+
+        for vertex in graph.verteces: #O(V)
+            if vertex.value == self.value:
+                print('A vertex must not be its own neighbor')
+            else:
+                weight = self.calculate_weight(vertex)
+                self.weights.append((vertex.value, weight))
+                if weight <= self.max_edge_length_km:
+                    self.neighbours.append(vertex)
+
+
+    def calculate_weight(self, vertex):
+        """Calculate the distance between two users 
+           and establish as weight"""
+        lat1 = self.latitude
+        len1 = self.length
+        lat2 = vertex.latitude
+        len2 = vertex.length
+        return utils.haversine(lat1,len1,lat2,len2)
+
+
+    def update_graph(self):
+        """When a new vertex is created, all its neighbours
+           have to update its adjacency list"""
+        for element in self.neighbours:
+            element.neighbours.append(self)
+            print('Graph update  successful')
