8e9bf222de24b84516a90a46d77bda1b188475e0
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-10-05 11:21:28 -0500

    add some changes on the configuration server and app routes

--- server.py
+++ server.py
@@ -1,80 +1,90 @@
+"""This is the main  file that control all aplication internal  flow"""
 #Flask
 from flask import Flask
 from flask import jsonify
-
+from configuration import Configuration
 #We_deal
 from models.graph import Graph, Vertex
-from data_base.load import db, Users
-from data_base.queries import get_persona
-
+from queries.queries import Get_work_area, Get_user_qualification, Get_user_latitude, Get_user_longitude
+from utils import Utils
 # Tools
 import joblib
-import numpy as np
-#import urllib.parse
-from decouple import config
+import pyodbc
+import os
 
-server =  config('Server_name')
-database = config('Data_base')
-username =  config('User_name')
-password = config('Password') 
-cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
 
 # Initializing app
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pymssql://{User_name}:{Password}@{Server_name}/{Data_base}"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db.init_app(app)
-
-
-
-
+app.config.from_object(Configuration)
+# Initializing data base conexion 
+server = os.environ['WEDEAL_SERVER'] 
+database = os.environ['WEDEAL_DATABASE'] 
+username = os.environ['WEDEAL_USER_NAME'] 
+password = os.environ['WEDEAL_PASSWORD'] 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+# Initializing Graph
+G = Graph('test','version1')
 
 
 # Routes
-@app.route('/user', methods=['GET'])
-def predict_user():
-    Users.query.all()
-    #user = Users.query.filter_by(id=2).first()
-    #print(user)
-    #model = joblib.load('./best_models/user_model_16.pkl')
-    #X_test = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,4.3])
-    #prediction = model.predict(X_test.reshape(1,-1))
-    #return jsonify({'user_work_area' : users.id_work_area }) # users.id_work_area
+@app.route('/user/<id_user>', methods=['GET'])
+def predict_user(id_user):
+    """ This route responses with a prediction based on user's work area
+        and qualification to use it  need as parameter a id user, 
+        at the end return a json with the response . 
+        Prediction use the best model with the best score 
+        located in best_models."""
+    work_area = Get_work_area(id_user, cnxn, 'user')
+    qualification = Get_user_qualification(id_user, cnxn)
+    X_test = Utils.Convert_user_data(work_area, qualification)
+    model = joblib.load('./best_models/user_model_16.pkl')
+    prediction = model.predict(X_test.reshape(1,-1))
+    return jsonify({'prediccion' : list(prediction)})
+    
 
-@app.route('/job_offer', methods=['GET'])
-def predict_job_offer():
+@app.route('/job_offer/<id_job_offer>', methods=['GET'])
+def predict_job_offer(id_job_offer):
+    """ This route responses with a prediction based on job offer work area
+        to use it  need as parameter a id job offer, 
+        at the end return a json with the response . 
+        Prediction use the best model with the best score 
+        located in best_models."""
+    work_area = Get_work_area(id_job_offer, cnxn, 'job_offer')
+    X_test = Utils.Convert_job_offer_data(work_area)
     model = joblib.load('./best_models/job_offer_model_16.pkl')
-    X_test = np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0])
     prediction = model.predict(X_test.reshape(1,-1))
     return jsonify({'prediccion' : list(prediction)})
 
-@app.route('/vertex', methods=['GET'])
-def new_vertex(user):
-    """ Create a new vertex. 
+@app.route('/vertex/<id_user>', methods=['GET'])
+def new_vertex(id_user):
+    """ This route create a new vertex. 
         Time complexity = O(2A)
         Space complexity = O(A+E)
         """
-    V = Vertex(user,latitude, length)
-    G = Graph()
-    G.verteces.append(V) 
-    V.search_neighbours() #O(V)
-    update_graph(V) #O(V)
-    print(f'New vertex of user {V.value} was created successful.')
+    for vertex in G.verteces:
+        if vertex.value == id_user:
+            response = jsonify({'User already exist' : id_user})
+        else:
+            latitude = Get_user_latitude(id_user, cnxn)
+            longitude = Get_user_longitude(id_user, cnxn)
+            V = Vertex(id_user,latitude, longitude)
+            G.verteces.append(V) 
+            V.search_neighbours(G) #O(V)
+            V.update_graph() #O(V)
+            response =jsonify({'New vertex was created succesful' : V.value})
+    return response
 
-@app.route('/location', methods=['GET'])
-def localization_filter (user):
-    """ Find  closer users based on his location. 
+@app.route('/location/<id_user>', methods=['GET'])
+def localization_filter ():
+    """ This route find  closer users of a user based on his location. 
         O(V)+O(sorted)
        """
-    G = Graph()
     for vertex in G.verteces:
-        if user == vertex.value:
+        if id_user == vertex.value:
             nearest_neighbours= sorted(vertex.weights, key=lambda x: x[1])
         else:
             print(f'user {user} is not in graph yet, first you have to add it in graph.')
-    return jsonify({'prediccion' : list(nearest_neighbours)}) 
+    return jsonify({'nearest_neighbours' : list(nearest_neighbours)}) 
 
 if __name__ == "__main__":
-    app.run(port=8080)
+    app.run()
