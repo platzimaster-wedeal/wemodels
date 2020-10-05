73ce42c6bb1c4785b60b844ece903fcdb49cd23e
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-10-05 11:58:13 -0500

    Add documentation on queries file

--- queries/queries.py
+++ queries/queries.py
@@ -1,17 +1,23 @@
+"""This file contain all database queries"""  
 
 def Get_user_work_area_id(id_user, cnxn):
+    """Receive an id user and return an id work area"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT id_work_area FROM users WHERE id=?", id_user).fetchone()
     id_work_area = row[0]
     return id_work_area
 
 def Get_job_offer_area_id(id_job_offer,cnxn):
+    """Receive an id job offer and return an id work area"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT id_work_area FROM work_area_jobs WHERE id_job_offer=?", id_job_offer).fetchone()
     id_work_area = row[0]
     return id_work_area
 
 def Get_work_area(id, cnxn, _type):
+    """Receive an id work area and return the work area name. 
+        Depending on where the request comes this function could take two flows, 
+        if  the request comes of  job offer or  user"""  
     if _type == 'job_offer':
         id = Get_job_offer_area_id(id,cnxn)
     else : 
@@ -23,6 +29,7 @@ def Get_work_area(id, cnxn, _type):
 
 
 def Get_user_qualification(id_user,cnxn):
+    """Receive an id user and return user's qualification """
     employee = Is_employee(id_user,cnxn)
     if employee == 1:
         id_employee = Get_employee_id(id_user,cnxn)
@@ -34,25 +41,28 @@ def Get_user_qualification(id_user,cnxn):
 
 
 def Is_employee(id_user,cnxn):
+    """Receive an id user and return 1 if user is employee or 0 if not"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT employee FROM users WHERE id=?", id_user).fetchone()
     employee = row[0]
     return employee
 
 def Is_employer(id_user,cnxn):
-    """me sobra"""
+    """Receive an id user and return 1 if user is employer or 0 if not"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT employer FROM users WHERE id=?", id_user).fetchone()
     employer = row[0]
     return employer
 
 def Get_employee_id(id_user,cnxn):
+    """Receive an id user and return an id employee"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT id FROM employees WHERE id_user=?", id_user).fetchone()
     id_employee = row[0]
     return id_employee
 
 def Get_employer_id(id_user,cnxn):
+    """Receive an id user and return an id employer"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT id FROM employers WHERE id_user=?", id_user).fetchone()
     id_employer = row[0]
@@ -60,24 +70,28 @@ def Get_employer_id(id_user,cnxn):
 
 
 def Get_qualificatio_employer(id_user_employer, cnxn):
+    """Receive an id employer and return his qualification"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT qualification FROM scores WHERE id_employer=?", id_user_employer).fetchone()
     qualification = row[0]
     return qualification
 
 def Get_qualificatio_employee(id_user_employee, cnxn):
+    """Receive an id employee and return his qualification"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT qualification FROM scores WHERE id_employee=?", id_user_employee).fetchone()
     qualification = row[0]
     return qualification
 
 def Get_user_latitude(id_user, cnxn):
+    """Receive an id user and return his latitude"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT latitude FROM users WHERE id=?", id_user).fetchone()
     latitude = row[0]
     return latitude
 
 def Get_user_longitude(id_user, cnxn):
+    """Receive an id user and return his longitude"""
     cursor = cnxn.cursor()
     row = cursor.execute("SELECT longitude FROM users WHERE id=?", id_user).fetchone()
     longitude = row[0]
