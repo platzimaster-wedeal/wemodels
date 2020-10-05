6dcaa7010f8301b321da1b1e1521d66563748266
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-10-04 19:24:04 -0500

    add new Convert data utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import joblib
 import math
 
@@ -29,3 +30,38 @@ class Utils:
         a=(math.sin(rad*dlat/2))**2 + math.cos(rad*lat1)*math.cos(rad*lat2)*(math.sin(rad*dlen/2))**2
         distancia = 2*R*math.asin(math.sqrt(a))
         return distancia 
+
+    def Convert_user_data(work_area, qualification):
+        areas = ['HR', 'Designing', 'Managment', 'Information Technology',
+        'Education', 'Advocate', 'Business Development',
+        'Health & Fitness', 'Agricultural', 'BPO', 'Sales', 'Consultant',
+        'Digital Media', 'Building & Construction', 'Automobile',
+        'Banking', 'Engineering', 'Food & Beverages', 'Finance', 'Apparel',
+        'Accountant', 'Architects', 'Public Relations', 'Arts', 'Aviation']
+        x = []
+        for area in areas:
+            if area == work_area:
+                x.append(1)
+            else:
+                x.append(0)
+        x.append(qualification)
+        return np.array(x)
+
+
+    def Convert_job_offer_data(work_area):
+        areas = ['HR', 'Designing', 'Managment', 'Information Technology',
+        'Education', 'Advocate', 'Business Development',
+        'Health & Fitness', 'Agricultural', 'BPO', 'Sales', 'Consultant',
+        'Digital Media', 'Building & Construction', 'Automobile',
+        'Banking', 'Engineering', 'Food & Beverages', 'Finance', 'Apparel',
+        'Accountant', 'Architects', 'Public Relations', 'Arts', 'Aviation']
+        x = []
+        for area in areas:
+            if area == work_area:
+                x.append(1)
+            else:
+                x.append(0)
+        return np.array(x)
+
+
+
