12dbcb31ef37e61515bd248db7522493ac448c3d
franco123 <58991258+juanfranco97@users.noreply.github.com>
2020-10-07 01:23:07 -0500

    Add two new route /bar and /bar/work_area and modify route /locatio

--- server.py
+++ server.py
@@ -4,8 +4,9 @@ from flask import Flask, make_response, jsonify
 from configuration import Configuration
 #We_deal
 from models.graph import Graph, Vertex
-from queries.queries import Get_work_area, Get_user_qualification, Get_user_latitude, Get_user_longitude
+from queries.queries import Get_work_area, Get_user_qualification, Get_user_latitude, Get_user_longitude, Get_users_based_on_work_area, Get_work_area_id
 from utils import Utils
+from models.bar import Users_list
 # Tools
 import joblib
 import pyodbc
@@ -23,6 +24,8 @@ password =  '9H#Md7m75@Pc'
 url_conexion='DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+password+''
 # Initializing Graph
 G = Graph('test','version1')
+# Initializing User List
+userslist = Users_list()
 
 
 # Routes
@@ -110,24 +113,87 @@ def new_vertex(id_user):
 
 @app.route('/location/<id_user>', methods=['GET'])
 def localization_filter (id_user):
-    """ This route find  closer users of a user based on his location. 
+    """ This route find  closer users of a user based on his location.
+        This route receive a id user and redirec to rute /bar 
         O(V)+O(sorted)
        """
     cnxn = pyodbc.connect(url_conexion)
     exist = Utils.User_already_exist(id_user, G)
     if exist == False:
-        response = make_response(
-            jsonify({'user is not in graph yet, first you have to add it in graph' : id_user }),
-            400,)
+        return response = make_response(
+               jsonify({'user is not in graph yet, first you have to add it in graph' : id_user }),
+               400,)
     else : 
         for vertex in G.verteces:
             if id_user == vertex.value:
                 nearest_neighbours= sorted(vertex.weights, key=lambda x: x[1])
-                response = make_response(
-                jsonify({'nearest_neighbours' : list(nearest_neighbours)}),
-                200,)
+                users = []
+                for element in nearest_neighbours:
+                    users.append(int(element[0]))
+                userslist.users2 = users
+                #response = make_response(
+                #jsonify({'nearest_neighbours' : list(nearest_neighbours)}),
+                # 200,)
     cnxn.close()
-    return response
+    return redirect(url_for('bar'))
+
+@app.route('/bar/<work_area>/<type_>',methods=['GET'])
+def Search_bar_filter(work_area, type_):
+    """ This route find an id users or id job offer 
+        based on a specific work area. 
+        This route recieve a work area name and a type, 
+        user type or job offer type  and redirect to route /bar """
+    cnxn = pyodbc.connect(url_conexion)
+    id_work_area = Get_work_area_id(work_area, cnxn)
+    if type_ == 'job_offer':
+      users = Get_users_based_on_work_area(id_work_area, cnxn, type_)
+    else:
+        users = Get_users_based_on_work_area(id_work_area, cnxn, type_)
+    if id_work_area == None or users == None:
+        return response = make_response(
+                        jsonify({'Someting was wrong with  your request' : work_area}),
+                        400,)
+    #elif type_ == 'user':
+    #    response = make_response(
+    #            jsonify({'users filter by work area' : users}),
+    #            200,)
+    #elif type_ == 'job_offer':
+    #    response = make_response(
+    #            jsonify({'job_offer filter by work area' : users}),
+    #            200,)
+    userslist.users1 = users
+    cnxn.close()
+    return redirect(url_for('bar'))
+    
+
+@app.route('/bar', methods=['GET'])
+def bar():
+    """ This route create a list of user based on 
+        two filters, filter by work area and filter by location.
+        this route  receive nothing and retorn a lists of 10 users
+        order by filters"""  
+    userslist.bar_filter()
+    users = userslist.users
+    if len(users) == 0:
+        return response = make_response(
+                jsonify({'there are not more imformation to send' : users}),
+                400,)
+    elif len(users) <= 10:
+        list1 = users[:10]
+        #users.pop(:10)
+        for i in range(10):
+            users.pop(i)
+        yield response = make_response(
+                jsonify({'users list is' : list1}),
+                200,)
+    else: 
+        return response = make_response(
+                jsonify({'users list is' : users}),
+                200,)
+        
+        
+
+
 
 if __name__ == "__main__":
     app.run()
